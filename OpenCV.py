516a1355d480f38930c4d95cc08f79fd109c87d5
qa-ap <cainstem@naver.com>
2022-06-07 04:19:40 +0900

    Update OpenCV.py

--- OpenCV.py
+++ OpenCV.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 
 image = np.full((512, 512, 3), 255, np.uint8)
 image = cv2.line(image, (0, 0), (255, 255), (255, 0, 0), 3)
-plt.axis('off')
+plt.axis('off') # remove axis
 plt.imshow(image)
 plt.show()
