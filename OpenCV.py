8cdb8e9471eab6a5cfa1ac077718bb0fc0c3cba8
LEE JINGU <cainstem@naver.com>
2022-06-07 03:57:16 +0900

    HAHA

---
+++ OpenCV.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = np.full((512, 512, 3), 255, np.uint8)
+image = cv2.line(image, (0, 0), (255, 255), (255, 0, 0), 3)
+plt.axis('off')
+plt.imshow(image)
+plt.show()
--- TEST.py
+++
@@ -1,3 +0,0 @@
-print('TEST')
-print('AVAILABLE?')
-print('PUSHPUSH')
