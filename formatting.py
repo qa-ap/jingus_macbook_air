1372f8c8e1cac7a7e008fa48b8949d3d56884a5f
qa-ap <cainstem@naver.com>
2022-06-07 04:27:06 +0900

    RKWMDK

---
+++ formatting.py
@@ -0,0 +1,10 @@
+my_str = 'My name is %s' % 'Young choi'
+print(my_str)
+
+print('%d and %d' % (1,2))
+print('%s' % 3.14)
+
+# format method 중괄호를 이요한다. / 자릿수는 어떻게 ?
+print('My name is %s' % '조희진')
+print('My name is {}'.format('조희진'))
+print('{} x {} = {}'.format(2,3,2*3))
