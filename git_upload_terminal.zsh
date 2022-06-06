7a9f4edcb83e7374688704e4eab8f82583999521
qa-ap <cainstem@naver.com>
2022-06-07 05:01:09 +0900

    Uploaded by Automated Script

--- git_upload_terminal.zsh
+++ git_upload_terminal.zsh
@@ -1,6 +1,7 @@
+#!/bin/zsh
 // 파일 수정 후 터미널에서 아래 명령어 입력 후 GitHub 새로고침
 // 추가하는 작업에서는 git init 할 필요 없음
 git add .
 git status
-git commit -m "Upload by Automated Script"
+git commit -m "Uploaded by Automated Script"
 git push origin master
